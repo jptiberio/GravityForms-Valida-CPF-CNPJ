e4da04e76bd9ae72c814a3360d84a0b9ac4138e7
João Pedro <jptiberio@icloud.com>
2017-10-27 13:48:53 -0200

    Initial Commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# GravityForms Valida CPF CNPJ
+Plugin que habilita o a validação de campo
+
+- Para utilizalo, adicione no label a classe "cpf_cnpj".
---
+++ gform_valida_cpf_cnpj.php
@@ -0,0 +1,75 @@
+<?php 
+
+/**
+ *
+ * Plugin Name: Gravity Forms Validação CPF CNPJ
+ * Description: Plugin que habilita a validação de CPF e CNPJ
+ * Author: JP Tibério
+ *
+ */
+
+require 'class-valida-cpf-cnpj.php';
+
+
+if ( is_plugin_active('advanced-custom-fields-pro/acf.php') ) {
+
+
+add_filter("gform_validation", "custom_validation");
+
+
+	function custom_validation( $validation_result){
+
+		$form = $validation_result['form'];
+
+		foreach( $form['fields'] as $field ) {
+
+
+			if ( $field->cssClass == 'cpf_cnpj' ) {
+
+				$field_value = rgpost( 'input_'.$field->id );
+
+
+				$cpf_cnpj = new ValidaCPFCNPJ($field_value);
+
+				if (!empty($cpf_cnpj)) {
+
+					$validado = $cpf_cnpj->valida();
+
+					if ( $validado != true ) {
+
+			            $field->validation_message = 'CPF/CNPJ Inválido';
+
+			        	$validation_result['is_valid'] = false;
+
+			            $field->failed_validation = true;
+
+			            break;
+					}
+
+				} else {
+
+		            $field->validation_message = 'CPF/CNPJ Inválido';
+
+		        	$validation_result['is_valid'] = false;
+
+		            $field->failed_validation = true;
+
+		            break;
+
+				}
+			    
+			}
+
+	    }
+
+
+	    $validation_result['form'] = $form;
+
+		return $validation_result;
+
+	}
+
+}
+
+
+?>
